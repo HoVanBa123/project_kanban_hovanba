0bb101693fba1f066c2493f7e24d4f922459706d
phuong nguyen <thaiphuongit25@gmail.com>
2020-08-31 13:44:57 +0700

    edit image

--- src/Component/LoginSignup.js
+++ src/Component/LoginSignup.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import './../Css/Login.css'
-import { Button, Container, Row, Col } from 'react-bootstrap';
+import { Button, Container, Row, Col, Image } from 'react-bootstrap';
 import {BrowserRouter as Router, NavLink  } from "react-router-dom";
 import RouterURL from './../RouterURL/RouterURL'
+import hero from './../images/hero-a.svg'
+import logo from './../images/trello-logo-blue.svg'
 
 class LoginSignup extends Component {
     render() {
@@ -11,7 +13,7 @@ class LoginSignup extends Component {
                 <div>
                     <div className='menu'>
                         <div>
-                                <img src='./../public/trello-logo-blue.svg' />
+                                <img src={logo} />
                                 <Button variant="warning">
                                     <li>
                                         <NavLink to="/login">Đăng nhập</NavLink>
@@ -32,7 +34,7 @@ class LoginSignup extends Component {
                                     <h4>Các bảng, danh sách, và thẻ của Trello cho phép các nhóm tổ chức và ưu tiên các dự án một cách vui vẻ, linh hoạt và xứng đáng.</h4>
                                 </Col>
                                 <Col>
-                                    <img src='./../public/hero-a.svg'  style={{width: '100%', height: '100%'}} />
+                                    <Image src={hero} thumbnail />
                                 </Col>
                             </Row>
                         </Container>
