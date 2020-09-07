fd029b83da2e37f1513c71c2ac04d3d30c408b99
Hồ Văn Bá <baho9393@gmail.com>
2020-09-07 08:22:50 +0700

    create login

--- src/Component/Login.js
+++ src/Component/Login.js
@@ -1,13 +1,68 @@
 import React, { Component } from 'react';
-
+import { Container, Row, Col, Image} from 'react-bootstrap';
 class Login extends Component {
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+            email: '',
+            password: ''
+        }
+        this.emailChange = this.emailChange.bind(this)
+        this.passwordChange = this.passwordChange.bind(this)
+    }
+    login() {
+        const email = this.refs.textEmail.value
+        const password =  this.refs.textPassword.value
+        const atr = localStorage.getItem('userSignup')
+        const emp = JSON.parse(atr)
+        const a = emp.find(x => x.email == email & x.password == password)
+        if(email == '' || password == '') {
+            alert('Vui lòng nhập đầy đủ thông tin')          
+        } else {
+            if(a == undefined) {
+                alert('Tài khoản hoặc mật khẩu không đúng, vui lòng kiểm tra lại')
+            } else {
+                alert('Đăng nhập thành công')
+            }
+        }
+    }
+    emailChange(event) {
+        this.setState({ email: event.target.value })
+    }
+    passwordChange(event) {
+        this.setState({ password: event.target.value })
+    }
     render() {
         return (
-            <div>
-                LOGIN
-            </div>
+           <div>
+               <Container fluid>
+                   <Row>
+                        <Col></Col>
+                        <Col className='interface'>
+                            <img src='/logo.svg' style={{height: '100px', width: '150px'}}/>
+                            <div className='item'>
+                                <p><b>Đăng nhập cho tài khoản bạn</b></p>
+
+                                <input type='email' placeholder='Nhập email' size='40'
+                                    value = {this.state.email}
+                                    ref = 'textEmail'
+                                    onChange={this.emailChange}
+                                />
+
+                                <input type='password' size='40' placeholder='nhập mật khẩu' 
+                                    value={this.state.password}
+                                    ref='textPassword'
+                                    onChange={this.passwordChange}
+                                /> <br />
+                                <button onClick={() => this.login()}>Đăng nhập</button>
+                            </div>
+                        </Col>
+                        <Col></Col>
+                   </Row>
+               </Container>
+           </div>
         );
     }
 }
-
 export default Login;
--- src/Component/Signup.js
+++ src/Component/Signup.js
@@ -3,24 +3,79 @@ import { Button, Container, Row, Col, FormControl, Form } from 'react-bootstrap'
 import './../Css/Signup.css'
 
 class Signup extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            usrerInfor: [],
+            email: '',
+            name: '',
+            password: ''
+        }
+        this.emailChange = this.emailChange.bind(this)
+        this.nameChange = this.nameChange.bind(this)
+        this.passwordChange = this.passwordChange.bind(this)
+    }
+    
+    signup() {
+        const email = this.refs.textEmail.value
+        const name = this.refs.textName.value
+        const password =  this.refs.textPassword.value
+        if(email == '' || name == '' || password == '') {
+            alert('Vui lòng kiểm tra lại và nhập đầy đủ thông tin')
+        }
+        const user = this.state.usrerInfor.concat([{email: email, name: name, password: password}])
+        localStorage.setItem('userSignup', JSON.stringify(user))
+        this.setState({
+            usrerInfor: user
+        });
+        alert('bạn đã đăng ký thành công')
+    }
+    emailChange(event) {
+        this.setState({ email: event.target.value })
+    }
+    nameChange(event) {
+        this.setState({ name: event.target.value })
+    }
+    passwordChange(event) {
+        this.setState({ password: event.target.value })
+    }
+
     render() {
         return (
             <div>
-               <Container>
+               <Container fluid>
                    <Row>
-                       <Col></Col>
-                       <Col className='signup'>
-                            <img />
-                            <div>
-                                <p>Sign up for your account</p>
-                                <Form>
-                                    <FormControl aria-label="Text input with checkbox" />
-                                    <FormControl aria-label="Text input with checkbox" />
-                                    <FormControl aria-label="Text input with checkbox" />
-                                </Form>
+                       <Col>
+                       </Col>
+
+                       <Col className='interface'>
+                            <img src='/logo.svg' style={{height: '100px', width: '150px'}}/>
+                            <div className='item'>
+                                <p><b>Đăng ký cho tài khoản bạn</b></p>
+                                <input type='email' placeholder='Enter email address' size='40'
+                                    value={this.state.email}
+                                    ref='textEmail'
+                                    onChange={this.emailChange}
+                                />
+
+                                <input type='text' size='40' placeholder='Enter full name'
+                                    value={this.state.name}
+                                    ref='textName'
+                                    onChange={this.nameChange}
+                                    
+                                />
+
+                                <input type='password' size='40' placeholder='Create password' 
+                                    value={this.state.password}
+                                    ref='textPassword'
+                                    onChange={this.passwordChange}
+                                /> <br />
+                                <button onClick={() => this.signup()}>Sign up</button>
                             </div>
                        </Col>
-                       <Col>ABC</Col>
+
+                       <Col>
+                       </Col>
                    </Row>
                </Container>
             </div>
